e871adc0a1f56370e2fd7da0847f7b2fc3b65ad0
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-07-18 23:05:49 +0530

    fetch data from api added search funcionality and Shimmer UI

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,30 +1,64 @@
-// import { useState } from "react";
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
 import cardData from "../../utils/cardData";
-import { useState } from "react"
+import Shimmer from "../Shimmer/Shimmer";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-    const [resList, setResList] = useState(cardData)
+  const [resList, setResList] = useState([]);
+  const [filteredResList, setFilteredResList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  
+  console.log("render")
+  const filterResListHandler = () => {
+    const filteredList = cardData.filter((res) => res.data.avgRating > 4);
+    //console.log(filteredList);
+    setResList(filteredList);
+  };
 
-    const filterResListHandler = () => {
-        const filteredList = cardData.filter((res) => res.data.avgRating > 4)
-        console.log(filteredList)
-        setResList(filteredList);
-    }
+  const onClickSearchHandler = () => {
+    console.log(resList[0].info.name.toLowerCase().includes(searchText.toLowerCase()));
+        const filteredList = resList.filter((data) => data.info.name.toLowerCase().includes(searchText.toLowerCase()))
+        setFilteredResList(filteredList)
+        console.log(filteredList);
+  }
 
-    return <div className="body">
+  const onChangeSearchHandler = (e) => {
+    setSearchText(e.target.value);
+  }
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6691565&lng=77.45375779999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    //console.log(response)
+    const json = await response.json();
+    //console.log(json)
+    console.log(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+    setResList(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+    setFilteredResList(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants)
+  };
 
-        <div className="filter-container">
-            <button className="filter-btn" onClick={filterResListHandler}>Top Rated Restaurants</button>
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return resList.length === 0? (<Shimmer/>):(
+    <div className="body">
+      <div className="filter-container">
+        <div className="search-container">
+            <input type="text" value = {searchText} onChange={onChangeSearchHandler}></input>
+            <button className= "search-btn" onClick={onClickSearchHandler}>Search</button>
         </div>
-        <div className="res-container">
-            {
-                resList.map((card) => (
-                    <RestaurantCard key={card.data.id} resData={card} />
-                ))
-            }
-        </div>
+        <button className="filter-btn" onClick={filterResListHandler}>
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredResList.map((card) => (
+          <RestaurantCard key={card.info.id} resData={card} />
+        ))}
+      </div>
     </div>
-}
+  );
+};
 
 export default Body;
--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -2,14 +2,14 @@ import { CDN_URL } from "../../utils/constant";
 
 const RestaurantCard = (props) => {
 
-    const { name, cloudinaryImageId, avgRating, cuisines, costForTwo, deliveryTime } = props.resData.data;
+    const { name, cloudinaryImageId, avgRating, cuisines, costForTwo} = props.resData.info;
     return <div className="res-card">
         <img src={CDN_URL + cloudinaryImageId} alt="" />
         <h3>{name}</h3>
         <p>{cuisines.join(", ")}</p>
-        <h4>{costForTwo / 100}</h4>
+        <h4>{costForTwo }</h4>
         <h4>{avgRating} star</h4>
-        <h4>{deliveryTime} minutes</h4>
+        {/* <h4>{sla.deliveryTime} minutes</h4> */}
     </div>
 }
 
