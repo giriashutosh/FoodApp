ce395e3caf4c7c1456212d3f87f2478d84ecf864
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-08-08 20:51:52 +0530

    add cart page

--- src/components/CartItems/CartItems.js
+++ src/components/CartItems/CartItems.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const CartDetail = () => {
-  useSelector((state) => state.cart.items)
+  const cartItems = useSelector((state) => state.cart.items)
   return (
     <div></div>
   )
